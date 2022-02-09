163b80b74188eb958b139ce85c51e1203553cda3
2SEHI <kreshi2@gmail.com>
2022-02-09 15:31:52 +0900

    2022/2/9 index 변경 소스

--- utils/modify_labels/change_index.py
+++ utils/modify_labels/change_index.py
@@ -2,10 +2,12 @@
 import glob, os, re
 
 def change_index(label_path):
-    old_helmet = "0"
-    old_vest = "1"
-    new_helmet = "1"
-    new_vest = "2"
+    index_dict = {"0": "1"}
+    # old_helmet = "0"
+    # # old_vest = "1"
+    # new_helmet = "1"
+    # # new_vest = "2"
+
 
     label_list = glob.glob(os.path.join(label_path, "*.txt"))
     print(len(label_list))
@@ -16,13 +18,11 @@ def change_index(label_path):
             lines = f.readlines()
             for line in lines:
                 new_line = ''
-                if line.startswith(old_vest):
-                    new_line = re.sub(f'^{old_vest}', new_vest, line)
-                    print(new_line)
-                elif line.startswith(old_helmet):
-                    new_line = re.sub(f'^{old_helmet}', new_helmet, line)
-                    print(new_line)
-                new_lines += new_line
+                for index_key, index_value in index_dict.items():
+                    if line.startswith(index_key):
+                        new_line = re.sub(f'^{index_key}', index_value, line)
+                        print(new_line)
+                    new_lines += new_line
             f.close()
 
         with open(file, 'w') as f:
@@ -30,6 +30,6 @@ def change_index(label_path):
             f.close()
 
 if __name__ == "__main__" :
-    path = "/Users/lfin/Downloads/safety_train_kaggle-helmet-vest.v1-original.yolov5pytorch/train/labels"
+    path = "/Users/lfin/Downloads/kaggle_helmet-person.v5-original_onlyhelmet.yolov5pytorch/train/labels"
     # new_label_path = "/Users/lfin/Downloads/safety_train_kaggle-helmet-vest.v1-original.yolov5pytorch/train/new_label_path"
     change_index(path)
