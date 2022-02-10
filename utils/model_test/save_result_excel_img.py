d354cdfc832737450dbffe70f1a4a98773fbbbdf
2SEHI <kreshi2@gmail.com>
2022-02-10 14:19:00 +0900

    2022/2/10 Sarah 좌표 거리계산 추가

--- utils/model_test/save_result_excel_img.py
+++ utils/model_test/save_result_excel_img.py
@@ -12,6 +12,7 @@ def save_test_result(opt):
     pred_root = opt.pred_root
     excel_root = opt.excel_root
     model_name = opt.model_name
+    img_size = float(opt.img_size)
 
     # ground truth 파일 리스트 취득
     gt_list = glob.glob(os.path.join(gt_root, '*.txt'))
@@ -26,10 +27,10 @@ def save_test_result(opt):
     pred_list.sort(reverse=False)
     # print(pred_list)
 
-    gt_columns = ['클래스명', '실제_x', '실제_y', '실제_w', '실제_h']
-    pred_columns = ['클래스명', '예측_x', '예측_y', '예측_w', '예측_h', '신뢰도']
+    gt_columns = ['실제 클래스', '실제_x', '실제_y', '실제_w', '실제_h']
+    pred_columns = ['예측 클래스', '예측_x', '예측_y', '예측_w', '예측_h', '신뢰도']
 
-    for gt_file in gt_list[:5]:
+    for gt_file in gt_list:
         # print(f'\n{gt_file}')
         gt_filename, exp = os.path.splitext(gt_file)
         # gt 파일명의 엑셀파일 불러옴
@@ -38,76 +39,77 @@ def save_test_result(opt):
 
         gt_path = f'{gt_root}/{gt_file}'
 
-        gt_data = pd.DataFrame(columns=gt_columns)
+        gt_data = pd.DataFrame(columns=gt_columns, dtype=np.float64)
         # gt 파일내용이 있을 경우 내용 읽어오기
         if not os.stat(gt_path).st_size == 0:
             print('gt 파일 내용이 존재합니다.')
-            gt_data = pd.read_csv(gt_path, sep=' ', header=None)
-            gt_data.columns = gt_columns
+            gt_data = pd.read_csv(gt_path, sep=' ', header=None, dtype=np.float64)
+        gt_data.columns = gt_columns
 
         # predict 파일 내용 읽어오기
         pred_path = f'{pred_root}/{gt_file}'
-        pred_data = pd.DataFrame(columns=pred_columns)
+        pred_data = pd.DataFrame(columns=pred_columns, dtype=np.float64)
 
         if os.path.exists(pred_path):
-            pred_data = pd.read_csv(pred_path, sep=' ', header=None)
-            pred_data.columns = pred_columns
-        results_df = pd.merge(left = gt_data , right = pred_data, how = "outer", on = "클래스명")
-        # results_df = pd.concat([gt_data, pred_data], ignore_index=True)
-        # gt와 pred가 없어도 모델명은 추가
-        df_cnt = len(results_df)
-        if len(results_df) < 2:
-            df_cnt = 1
-        results_df.insert(0, '모델명', [model_name] * df_cnt)
+            pred_data = pd.read_csv(pred_path, sep=' ', header=None, dtype=np.float64)
+            pred_cnt = len(pred_data)
+            if pred_cnt < 2:
+                pred_cnt = 1
 
+            pred_data.columns = pred_columns
+        # gt와 pred 합치기
+        results_df = pd.merge(gt_data, pred_data, left_on='실제 클래스', right_on='예측 클래스', how='outer')
+
+        # 모델명 컬럼 추가
+        gt_cnt = len(results_df)
+        if gt_cnt < 2:
+            gt_cnt = 1
+        model_df = pd.DataFrame(np.array([model_name] * gt_cnt), columns=["모델명"])
+        results_df = pd.concat([model_df, results_df], axis=1)
+
+        if len(pred_data) > 0:
+            # 신뢰도로 sort
+            grouped = results_df.groupby(['모델명', '예측 클래스'])
+            def conf_sort(df, column='신뢰도'):
+                return df.sort_values(by=column, ascending=False)
+            results_df = grouped.apply(conf_sort)
+        # TODO 좌표가 -1인 경우 0으로 채우기
+        results_df.fillna({'실제_x': 0, '예측_x': 0, '실제_y': 0, '예측_y': 0,
+                           '실제_w': 0, '예측_w': 0, '실제_h': 0, '예측_h': 0}, inplace=True)
+
+        # 좌표거리 계산
+        new_result = coo_dist(results_df, img_size)
+        # 영역크기차이 계산
+        new_result = coo_dist(results_df, img_size)
+
+
+        # 기존 엑셀데이터와 합치기
         dfs = [excel_pd, results_df]
         # df = pd.concat( dfs,axis=0,ignore_index=True)
-        result3_in = pd.concat(dfs, axis=0, join='inner', ignore_index=True)  # 열방향(axis=1), 교집합(inner)
+        result3_in = pd.concat(dfs, axis=0, join='inner', ignore_index=True)
         result3_in.insert(0, 'num', np.arange(len(result3_in))+1)
         print('<<<<<<<<<<<result3_in>>>>>>>>>>>>>')
-        # print(result3_in)
-        new_result = calualate(result3_in, 416, 416)
 
         result3_in.to_excel(f'{excel_root}/{gt_filename}.xlsx')
 
-def calualate(coo_df, img_w, img_h):
+def coo_dist(coo_df, img_size):
     new_coo_df = coo_df.copy()
-    new_coo_df['실제_x'] = new_coo_df['실제_x'] * img_w
-    new_coo_df['실제_y'] = new_coo_df['실제_y'] * img_h
-    new_coo_df['실제_w'] = new_coo_df['실제_w'] * img_w
-    new_coo_df['실제_h'] = new_coo_df['실제_h'] * img_h
-    new_coo_df['예측_x'] = new_coo_df['예측_x'] * img_w
-    new_coo_df['예측_y'] = new_coo_df['예측_y'] * img_h
-    new_coo_df['예측_w'] = new_coo_df['예측_w'] * img_w
-    new_coo_df['예측_h'] = new_coo_df['예측_h'] * img_h
-    new_coo_df.fillna(0, inplace=True)
+    new_coo_df['실제_x'] = new_coo_df['실제_x'] * img_size
+    new_coo_df['실제_y'] = new_coo_df['실제_y'] * img_size
+    new_coo_df['실제_w'] = new_coo_df['실제_w'] * img_size
+    new_coo_df['실제_h'] = new_coo_df['실제_h'] * img_size
+    new_coo_df['예측_x'] = new_coo_df['예측_x'] * img_size
+    new_coo_df['예측_y'] = new_coo_df['예측_y'] * img_size
+    new_coo_df['예측_w'] = new_coo_df['예측_w'] * img_size
+    new_coo_df['예측_h'] = new_coo_df['예측_h'] * img_size
+    # new_coo_df.fillna(0, inplace=True)
     print(new_coo_df[['실제_x', '실제_y', '예측_x', '예측_y']])
-    new_coo_df['각 중심점의 차이'] = euclidean_distance(new_coo_df['실제_x']-new_coo_df['예측_x'],
-                                                    new_coo_df['실제_y']-new_coo_df['예측_y'])
-    print(new_coo_df['각 중심점의 차이'])
-    # p = new_coo_df[['실제_x', '예측_x']]
-    # q = new_coo_df[['실제_y', '예측_y']]
-    # result3 = np.array(list(map(math.dist, p, q)))
-    # print(p, q)
-    # result3 = math.dist( p, q)
-    # print('!!!!!!')
-    # # new_coo_df['각 중심점의 차이'] =
-    # print(result3)
-    # new_coo_df['각 중심점의 차이'] = math.dist(p, q)
-    # print(new_coo_df['각 중심점의 차이'])
+    x = new_coo_df['실제_x']-new_coo_df['예측_x']
+    y = new_coo_df['실제_y']-new_coo_df['예측_y']
+    new_coo_df['각 중심점의 차이'] = np.sqrt(x ** 2 + y ** 2)
     return new_coo_df
 
-def euclidean_distance(x, y):
-    # print(x**2 + y**2)
-    # print(np.sqrt(x ** 2 + y ** 2))
-    return np.sqrt(x ** 2 + y ** 2)
-
-    # new_coo_w = new_coo_df[['실제_x', '실제_w', '예측_x', '예측_w']] * img_w
-    # print(new_coo_df)
-    # new_coo_y = new_coo_df[['실제_y', '실제_h', '예측_y', '예측_h']] * img_h
-    # # new_coo_df = new_coo_df[new_coo_df['실제_x'] > 0]['실제_x'] * img_w
-    # print(new_coo_df)
-
+# def get_area():
 
 def get_excel(excel_root, file_name):
     if os.path.exists(f'{excel_root}/{file_name}') :
@@ -135,6 +137,7 @@ if __name__ == "__main__" :
     parser.add_argument('--pred_root', type=str, default='/Users/lfin/Downloads/model_test/20__3090_20220207_kaggle_helmet_vest/labels', help='예측txt파일이 저장된 위치')
     parser.add_argument('--excel_root', type=str, default='/Users/lfin/Downloads/model_test/10_excel',  help="excel파일을 저장할 위치")
     parser.add_argument('--model_name', type=str, default='3090_kdn2',  help="모델명")
+    parser.add_argument('--img_size', type=str, default='416', help="이미지사이즈")
     opt = parser.parse_args()
     # get_multicolumns()
     save_test_result(opt)
